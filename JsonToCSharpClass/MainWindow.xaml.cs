deddafddc1c3e7627e05f9bc13c5b74931aaccde
Xioa <132796255@qq.com>
2024-12-21 13:35:47 +0800

    Main

---
+++ JsonToCSharpClass/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using JsonToCSharpClass.ViewModels;
+using System.Windows;
+
+namespace JsonToCSharpClass
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.DataContext = new MainViewModel();
+            InitializeComponent();
+        }
+    }
+}
